a274a65e8915fb92d73b4cd6ab7edac56f5f3cf7
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2024-04-10 10:57:55 -0400

    Review and override file_set_helper.rb

--- app/helpers/hyku_knapsack/application_helper.rb
+++ app/helpers/hyku_knapsack/application_helper.rb
@@ -2,5 +2,20 @@
 
 module HykuKnapsack
   module ApplicationHelper
+    # OVERRIDE Hyrax::FileSetHelper#media_display_partial
+    #
+    # changed from hyrax 3.4.1 to change audio and video items to use default partial
+    def media_display_partial(file_set)
+      'hyrax/file_sets/media_display/' +
+        if file_set.image?
+          'image'
+        elsif file_set.pdf?
+          'pdf'
+        elsif file_set.office_document?
+          'office_document'
+        else
+          'default'
+        end
+    end
   end
 end
