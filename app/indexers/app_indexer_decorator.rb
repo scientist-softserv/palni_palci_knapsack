c0a37d5fb63017697f1ce9fce4a1230f84220786
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2024-04-10 10:19:57 -0400

    Move functionality to Hyku prime

--- app/indexers/app_indexer_decorator.rb
+++ app/indexers/app_indexer_decorator.rb
@@ -3,9 +3,6 @@
 module AppIndexerDecorator
   def generate_solr_document
     super.tap do |solr_doc|
-      solr_doc['bulkrax_identifier_tesim'] = object.bulkrax_identifier if object.respond_to?(:bulkrax_identifier)
-      # TODO: Extract SortTitle to Hyku?
-      solr_doc['title_ssi'] = SortTitle.new(object.title.first).alphabetical
       solr_doc['admin_note_tesim'] = object.admin_note if object.respond_to?(:admin_note)
     end
   end
