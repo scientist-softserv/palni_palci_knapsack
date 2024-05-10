e3a5bc2c2546de123af8f862ce8301b79ca9971f
Shana Moore <shana@scientist.com>
2024-05-10 11:51:06 -0700

    Update application_helper.rb

--- app/helpers/hyku_knapsack/application_helper.rb
+++ app/helpers/hyku_knapsack/application_helper.rb
@@ -17,5 +17,28 @@ module HykuKnapsack
           'default'
         end
     end
+
+    ##
+    # @return [String] the URL to use for searching across all tenants.
+    #
+    # @see .cross_tenant_search_host
+    # @see https://github.com/scientist-softserv/palni-palci/issues/947
+    def self.cross_tenant_search_url
+      # Do not include the scheme (e.g. http or https) but instead let the browser apply the correct
+      # scheme.
+      "//#{cross_tenant_search_host}/catalog"
+    end
+    
+    ##
+    # @api private
+    #
+    # @return [String] the host (e.g. "search.hykucommons.org") for cross-tenant search.
+    def self.cross_tenant_search_host
+      # I'm providing quite a few fallbacks, as this URL is used on the first page you'll see in a
+      # new Hyku instance.
+      ENV["HYKU_CROSS_TENANT_SEARCH_HOST"].presence ||
+      Account.where(search_only: true).limit(1).pluck(:cname)&.first ||
+        "search.hykucommons.org"
+    end
   end
 end
