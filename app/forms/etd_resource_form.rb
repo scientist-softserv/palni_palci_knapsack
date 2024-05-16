7051ffbfe3a63a2d0b220cca5e6b7f4f91938fd0
Kirk Wang <kirk.wang@scientist.com>
2024-05-15 19:02:40 -0700

    Correct EtdResoource properties and forms
    This commit will add the EtdResoource properties to match Etd

--- app/forms/etd_resource_form.rb
+++ app/forms/etd_resource_form.rb
@@ -6,8 +6,11 @@
 # @see https://github.com/samvera/hyrax/wiki/Hyrax-Valkyrie-Usage-Guide#forms
 # @see https://github.com/samvera/valkyrie/wiki/ChangeSets-and-Dirty-Tracking
 class EtdResourceForm < Hyrax::Forms::PcdmObjectForm(EtdResource)
-  include Hyrax::FormFields(:basic_metadata)
+  # Commented out basic_metadata because these terms were added to etd_resource so we can customize it.
+  # include Hyrax::FormFields(:basic_metadata)
   include Hyrax::FormFields(:etd_resource)
+  include Hyrax::FormFields(:with_pdf_viewer)
+  include Hyrax::FormFields(:with_video_embed)
 
   # Define custom form fields using the Valkyrie::ChangeSet interface
   #
--- app/models/etd_resource.rb
+++ app/models/etd_resource.rb
@@ -3,12 +3,15 @@
 # Generated via
 #  `rails generate hyrax:work_resource EtdResource`
 class EtdResource < Hyrax::Work
-  include Hyrax::Schema(:basic_metadata)
+  # Commented out basic_metadata because these terms were added to etd_resource so we can customize it.
+  # include Hyrax::Schema(:basic_metadata)
   include Hyrax::Schema(:etd_resource)
   include Hyrax::Schema(:with_pdf_viewer)
   include Hyrax::Schema(:with_video_embed)
   include Hyrax::ArResource
   include Hyrax::NestedWorks
+
+  Hyrax::ValkyrieLazyMigration.migrating(self, from: Etd)
 
   include IiifPrint.model_configuration(
     pdf_split_child_model: GenericWorkResource,
