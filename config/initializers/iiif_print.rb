5363e34c6c9b0f1dea28a88cc38f48970666a9c6
LaRita Robinson <larita@scientist.com>
2024-06-25 16:33:41 -0400

    Bring along visibility attributes
    Child works need the visiblity of the parents.

--- config/initializers/iiif_print.rb
+++ config/initializers/iiif_print.rb
@@ -23,12 +23,38 @@ IiifPrint.config do |config|
   # config.default_iiif_manifest_version = 3
 
   config.child_work_attributes_function = lambda do |parent_work:, admin_set_id:|
-    {
+    embargo = parent_work.embargo
+    lease = parent_work.lease
+    embargo_params = {}
+    lease_params = {}
+    visibility_params = {}
+
+    if embargo
+      embargo_params = {
+        visibility: 'embargo',
+        visibility_after_embargo: embargo.visibility_after_embargo,
+        visibility_during_embargo: embargo.visibility_during_embargo,
+        embargo_release_date: embargo.embargo_release_date
+      }
+    elsif lease
+      lease_params = {
+        visibility: 'lease',
+        visibility_after_lease: lease.visibility_after_lease,
+        visibility_during_lease: lease.visibility_during_lease,
+        lease_release_date: lease.lease_expiration_date
+      }
+    else
+      visibility_params = { visibility: parent_work.visibility.to_s }
+    end
+
+    params = {
       admin_set_id: admin_set_id.to_s,
       creator: parent_work.creator.to_a,
       keyword: parent_work.keyword,
       rights_statement: parent_work.rights_statement.to_a,
       is_child: true
     }
+
+    params.merge!(embargo_params).merge!(lease_params).merge!(visibility_params)
   end
 end
