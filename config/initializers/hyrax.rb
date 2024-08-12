13760515cc380b943f5d4baffccbc800d4bbcb43
Kirk Wang <kirk.wang@scientist.com>
2024-08-12 12:31:15 -0700

    ⚙️ Update submodule and remove decorator
    We no longer need the SimpleSchemaLoaderDecorator as we are now using
    the Hyrax.config to load the schema.

--- config/initializers/hyrax.rb
+++ config/initializers/hyrax.rb
@@ -20,6 +20,8 @@ Rails.application.config.after_initialize do
     # config.enable_ffmpeg = false
 
     # config.branding_path = ENV.fetch('HYRAX_BRANDING_PATH', Rails.root.join('public', 'branding'))
+
+    config.simple_schema_loader_config_search_paths.unshift(HykuKnapsack::Engine.root)
   end
 
   # Ensure that valid_child_concerns are set with all the curation concerns including
